a4eac799f063bc11842cebecf00e11922770180a
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2023-01-13 02:46:52 +0500

    made nice characters script, relocated arrays

--- options.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    againOptions:{
-        reply_markup: JSON.stringify({
-            inline_keyboard:[
-                [{text:'Еще одна', callback_data:'/again'}]
-            ]
-        })
-    }
-}
